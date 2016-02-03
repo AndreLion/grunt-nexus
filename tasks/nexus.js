4f6b8f6e3c02ad146e749fb30164aff8c42ff226
Andre <andre.github@gmail.com>
2016-02-03 15:44:36 +0800

    add classifier and unpack option

--- tasks/nexus.js
+++ tasks/nexus.js
@@ -1,6 +1,7 @@
 var temp     = require('temp');
 var download = require('./util/download');
 var extract  = require('./util/extract');
+var fs       = require('fs');
 
 module.exports = function(grunt) {
 
@@ -8,6 +9,7 @@ module.exports = function(grunt) {
 
     // merge task-specific and/or target-specific options with these defaults
     var options = this.options({
+      unpack: true,
       extension: '.tar.gz'
     });
 
@@ -35,14 +37,33 @@ module.exports = function(grunt) {
         id: dependency,
         version: data.dependencies[dependency]
       };
-      var file = artifact.id + '-' + artifact.version + data.extension;
+      var file = [artifact.id, artifact.version];
+      if (data.classifier !== undefined) {
+        file.push(data.classifier);
+      }
+      file = file.join('-') + data.extension;
+      var folder = data.baseUrl + '/' + data.repository + '/' + data.groupId + '/' + artifact.id + '/' + artifact.version;
       var uri = data.baseUrl + '/' + data.repository + '/' + data.groupId + '/' + artifact.id + '/' + artifact.version + '/' + file;
       var dir = data.path + '/' + artifact.id;
       var tempPath = temp.path({prefix: 'grunt-nexus-', suffix: data.extension});
 
+      grunt.log.ok('Downloading ' + uri);
       download(uri, tempPath, data.strictSSL)
       .then(function() {
-        return extract(tempPath, dir);
+        if (data.unpack) {
+          return extract(tempPath, dir);
+        } else {
+          console.log('I need to move the zip file from',tempPath, 'to ', data.path + '/' + file);
+          var source = fs.createReadStream(tempPath);
+          var dest = fs.createWriteStream(data.path + '/' + file);
+          source.pipe(dest);
+          source.on('end', function() {
+            grunt.log.ok('Successfully downloaded ' + file);
+          });
+          source.on('error', function(err) {
+            grunt.log.error('Error when '+error.when+' '+artifact.id+':'+artifact.version+': '+error.message);
+          });
+        }
       })
       .then(function() {
         grunt.log.ok('Successfully installed '+artifact.id+':'+artifact.version);
